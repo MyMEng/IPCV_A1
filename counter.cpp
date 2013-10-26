21df407359f7304011888787ab2273e12d9071d1
Kacper <k.sokol.2011@my.bristol.ac.uk>
2013-10-26 06:11:51 +0100

    swapped i and j in 176

--- counter.cpp
+++ counter.cpp
@@ -6,15 +6,19 @@
 #define NIMAGES 1
 #define PI 3.14159265
 
-#define IMGTHRESHOLD 50
-#define HOUGHX 441 //dimension of Hough Space in x direction
-#define HOUGHY 341 //dimension of Hough Space in y direction
-#define RMIN 30
+#define IMGTHRESHOLD 60
+#define HOUGHX 341 //dimension of Hough Space in rows
+#define HOUGHY 441 //dimension of Hough Space in cols
+#define RMIN 25
 #define RMAX 50 //maximal radius of circle
-#define HOUGHTHRESHOLD 11
+#define HOUGHTHRESHOLD 50
+
+#define MEDIANFILTERWIDTH 3
+#define MEDIANFILTERHEIGHT 6
 
 using namespace cv;
 using namespace std;
+
 
 void sobel(const cv::Mat& image, int number, cv::Mat& xDeriv, cv::Mat& yDeriv, cv::Mat& grad, cv::Mat& arc)
 {
@@ -85,7 +89,7 @@ void sobel(const cv::Mat& image, int number, cv::Mat& xDeriv, cv::Mat& yDeriv, c
 	{
 		for(int j =0; j < divided.cols; j++)
 		{	
-			arc.at<double>(i, j) = (double)atan(divided.at<double>(i, j)) ;//* 180 / PI;
+			arc.at<double>(i, j) = (double)atan2(xDeriv.at<double>(i,j), yDeriv.at<double>(i,j)) ;//* 180 / PI;
 		}
 	} 
 
@@ -147,6 +151,7 @@ cv::Mat trshld( const int imageID, const cv::Mat& xDeriv, const cv::Mat& yDeriv,
 
 void hough( const int imageID, cv::Mat& grad, const cv::Mat& arc, cv::Mat& img)
 {
+	std::cout << "Achieved this stage 0" << std::endl ;
 	std::ostringstream windowName;
 	cv::vector<cv::Vec3d> circles ; //x,y,r
 	std::vector<std::vector<std::vector<int> > > houghSpace (HOUGHX, std::vector<std::vector<int> > (HOUGHY, std::vector<int>(RMAX-RMIN, 0) ) ) ;
@@ -168,21 +173,24 @@ void hough( const int imageID, cv::Mat& grad, const cv::Mat& arc, cv::Mat& img)
 				for (int r = RMIN; r < RMAX; ++r)
 				{
 					//shifted by RMAX to make scaling easier task
-					double x1 = i+r*cos(arc.at<double>(i,j)) + RMAX ;
-					double x2 = i-r*cos(arc.at<double>(i,j)) + RMAX ;
-					double y1 = j+r*sin(arc.at<double>(i,j)) + RMAX ;
-					double y2 = j-r*sin(arc.at<double>(i,j)) + RMAX ;
+					double x1 = j+r*cos(arc.at<double>(i,j)) + RMAX ;
+					double x2 = j-r*cos(arc.at<double>(i,j)) + RMAX ;
+					double y1 = i+r*sin(arc.at<double>(i,j)) + RMAX ;
+					double y2 = i-r*sin(arc.at<double>(i,j)) + RMAX ;
 
 					//remember about transforming form i, j coordinates to hough coordinate
 					int rowMin = 0 ; // 0 - RMAX ;
 					int colMin = 0 ; // 0 - RMAX ;
-					int rowMax = grad.rows + 2*RMAX ; // grad.rows + RMAX ;
-					int colMax = grad.cols + 2*RMAX ; // grad.cols + RMAX ;
+					int rowMax = grad.rows -1 + 2*RMAX ; // grad.rows + RMAX ;
+					int colMax = grad.cols -1 + 2*RMAX ; // grad.cols + RMAX ;
 
-					int sx1 = (int) ( ( x1 * HOUGHX / (rowMax - rowMin) ) ) ;
-					int sx2 = (int) ( ( x2 * HOUGHX / (rowMax - rowMin) ) ) ;
-					int sy1 = (int) ( ( y1 * HOUGHY / (colMax - colMin) ) ) ;
-					int sy2 = (int) ( ( y2 * HOUGHY / (colMax - colMin) ) ) ;
+					int sx1 = round( ( ( x1 * (HOUGHX-1) / (rowMax - rowMin) ) ) ) ;
+					int sx2 = round( ( ( x2 * (HOUGHX-1) / (rowMax - rowMin) ) ) ) ;
+					int sy1 = round( ( ( y1 * (HOUGHY-1) / (colMax - colMin) ) ) ) ;
+					int sy2 = round( ( ( y2 * (HOUGHY-1) / (colMax - colMin) ) ) ) ;
+
+					if ( sx1 > HOUGHX || sx2 > HOUGHX || sy1 > HOUGHY || sy2 > HOUGHY )
+					std::cout << sx1 << " " << sx2 << " " << sy1 << " " << sy2 << std::endl;
 
 					//increase hough space for x1, y1, r
 					houghSpace[sx1][sy1][r-RMIN] += 1 ;
@@ -195,16 +203,16 @@ void hough( const int imageID, cv::Mat& grad, const cv::Mat& arc, cv::Mat& img)
 					// flatHoughSpace[(int)x1][(int)y2] += 1 ;
 					// flatHoughSpace[(int)x2][(int)y1] += 1 ;
 					// flatHoughSpace[(int)x2][(int)y2] += 1 ;
-					flatHoughSpace.at<double>( (int) ( ( x1 * grad.rows / (rowMax - rowMin) ) ), (int) ( ( y1 * grad.cols / (colMax - colMin) ) ) ) += 1 ;
-					flatHoughSpace.at<double>( (int) ( ( x1 * grad.rows / (rowMax - rowMin) ) ), (int) ( ( y2 * grad.cols / (colMax - colMin) ) ) ) += 1 ;
-					flatHoughSpace.at<double>( (int) ( ( x2 * grad.rows / (rowMax - rowMin) ) ), (int) ( ( y1 * grad.cols / (colMax - colMin) ) ) ) += 1 ;
-					flatHoughSpace.at<double>( (int) ( ( x2 * grad.rows / (rowMax - rowMin) ) ), (int) ( ( y2 * grad.cols / (colMax - colMin) ) ) ) += 1 ;
+					flatHoughSpace.at<double>( round ( ( x1 * (flatHoughSpace.rows-1) / (rowMax - rowMin) ) ), round ( ( y1 * (flatHoughSpace.cols-1) / (colMax - colMin) ) ) ) += 1 ;
+					flatHoughSpace.at<double>( round ( ( x1 * (flatHoughSpace.rows-1) / (rowMax - rowMin) ) ), round ( ( y2 * (flatHoughSpace.cols-1) / (colMax - colMin) ) ) ) += 1 ;
+					flatHoughSpace.at<double>( round ( ( x2 * (flatHoughSpace.rows-1) / (rowMax - rowMin) ) ), round ( ( y1 * (flatHoughSpace.cols-1) / (colMax - colMin) ) ) ) += 1 ;
+					flatHoughSpace.at<double>( round ( ( x2 * (flatHoughSpace.rows-1) / (rowMax - rowMin) ) ), round ( ( y2 * (flatHoughSpace.cols-1) / (colMax - colMin) ) ) ) += 1 ;
 
 				}
 			}
 		}
 	}
-	std::cout << "Achieved this stage" << std::endl ;
+	std::cout << "Achieved this stage2" << std::endl ;
 
 	//take logs
 	for (int i = 0; i < flatHoughSpace.rows; ++i)
@@ -241,7 +249,7 @@ void hough( const int imageID, cv::Mat& grad, const cv::Mat& arc, cv::Mat& img)
 				if ( houghSpace[i][j][r] > HOUGHTHRESHOLD )
 				{
 					//once again rescale
-					circles.push_back( cv::Vec3d( (i * grad.rows / HOUGHX), (j * grad.cols / HOUGHY), r+RMIN ) ) ;
+					circles.push_back( cv::Vec3d( round(i * (img.rows-1) / (HOUGHX-1) ), round(j * (img.cols-1) / (HOUGHY-1) ), r+RMIN ) ) ;
 				}
 			}
 		}
@@ -256,14 +264,14 @@ void hough( const int imageID, cv::Mat& grad, const cv::Mat& arc, cv::Mat& img)
 	    int radius = tmp[2];
 
 	    // providing a negative number will create a filled circle
-	    int thickness = 4;
+	    int thickness = 1;
 
 	    // 8 connected line
 	    // ( also there is a 4 connected line and CVAA which is an anti aliased line )
 	    int linetype = 8; 
 
 	    // here is where we define the center of the circle
-		cv::Point center( (int)tmp[0], (int)tmp[1] );
+		cv::Point center( round(tmp[0]), round(tmp[1]) );
 		cv::circle ( img , center , radius , redColour , thickness , linetype );
 
 		circles.pop_back() ;
